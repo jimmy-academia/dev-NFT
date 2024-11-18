63f7d0dca37db7d570a072852fb531f173687b38
cyyeh <d09942009@ntu.edu.tw>
2024-11-18 17:34:57 +0800

    just another commitment to greatness

--- src/experiments/main_exp.py
+++ src/experiments/main_exp.py
@@ -40,8 +40,8 @@ def run_experiments():
                     Solver = get_solver(args, _method)
                     start_time = time.time()
                     Solver.solve() # recommend pricing and NFT purchase for each user
+                    Solver.evaluate() # calculate buyer utility, seller revenue
                     runtime = time.time() - start_time
-                    Solver.evaluate() # evaluate buyer utility, seller revenue
                     Result = {
                         'runtime': runtime,
                         'seller_revenue': Solver.seller_revenue,
@@ -55,57 +55,3 @@ def run_experiments():
                     print(f'seller_revenue {Solver.seller_revenue.item()} buyer_utilities {Solver.buyer_utilities[:, :dd].sum(1).mean().item()}')
                     print('______________________________________experiment done.')
 
-# import shutil
-# def run_buyer_utility():
-
-#     print('>>> (main_exp.py) experiments for buyer utilities: \n')
-    
-#     args = default_args()
-#     args.setN = None
-#     args.setM = None
-#     args.checkpoint_dir = args.ckpt_dir / 'buyer_util'
-#     args.checkpoint_dir.mkdir(parents=True, exist_ok=True)
-#     args.read_pricing_dir = args.ckpt_dir / 'main_exp'
-#     if not args.read_pricing_dir.exists():
-#         print(f'No results found in {args.read_pricing_dir}, please do run_revenue_experiments first.')
-#         return
-
-#     msg = f'''
-#          {nft_project_names[:4]} ... 
-#          ... {nft_project_names[4:]} 
-#         x {Baseline_Methods} 
-#         x {Breeding_Types}'''
-#     print(msg)
-
-#     for nft_project_name in nft_project_names:
-#         args.nft_project_name = nft_project_name
-    
-#         for _breeding in Breeding_Types:
-#             # get pricing from BANTER...
-#             pricing_file = args.read_pricing_dir / f'{nft_project_name}_BANTER_{_breeding}.pth'
-#             pricing = torch.load(pricing_file)['pricing'].to(args.device)
-
-#             for _method in Baseline_Methods:
-#                 result_file = args.checkpoint_dir / f'{nft_project_name}_{_method}_{_breeding}.pth'
-#                 if result_file.exists():
-#                     print(f'{result_file} exists, experiment is completed.')
-#                 else:
-#                     if _method == 'BANTER':
-#                         shutil.copy(pricing_file, result_file)
-#                         print(f'copy result for BANTER...')
-#                         continue
-#                     print(f'running [{nft_project_name}, {_method}, {_breeding}] experiment...')
-#                     args.breeding_type = _breeding
-#                     Solver = get_solver(args, _method)
-#                     Solver.solve(set_pricing=pricing)
-#                     Solver.evaluate()
-#                     Result = {
-#                         'seller_revenue': Solver.seller_revenue,
-#                         'buyer_utilities': Solver.buyer_utilities, 
-#                         'pricing': pricing, 
-#                         'holdings': Solver.holdings, 
-#                         'buyer_budgets': Solver.buyer_budgets,
-#                         'nft_counts': Solver.nft_counts,
-#                     }
-#                     torch.save(deep_to_cpu(Result), result_file)
-#                     print(f'[{nft_project_name}, {_method}, {_breeding}] buyer utility experiment done.')
